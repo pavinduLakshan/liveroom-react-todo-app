457bb3e80cfb10eb8cea196ad622ff9fb439138a
pavinduLakshan <174084F@uom.lk>
2020-07-13 14:23:48 +0530

    Updates

---
+++ precache-manifest.a2c2324498e3f6f79330d7cdd611498e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f30ef1f54bf7f5edc96a2ea1c441cb69",
+    "url": "/liveroom-react-todo-app/index.html"
+  },
+  {
+    "revision": "3fea5f661b0a5465c47c",
+    "url": "/liveroom-react-todo-app/static/css/main.e5982c47.chunk.css"
+  },
+  {
+    "revision": "cd17664756de5ab90aee",
+    "url": "/liveroom-react-todo-app/static/js/2.44655a66.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/liveroom-react-todo-app/static/js/2.44655a66.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3fea5f661b0a5465c47c",
+    "url": "/liveroom-react-todo-app/static/js/main.9497e748.chunk.js"
+  },
+  {
+    "revision": "0c0a50ee70beedfacc3e",
+    "url": "/liveroom-react-todo-app/static/js/runtime-main.44645848.js"
+  }
+]);
