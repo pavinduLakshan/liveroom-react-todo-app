ed6938e7dc4b43ad4cbf1e364f1b54be35609c17
pavinduLakshan <174084F@uom.lk>
2020-07-14 18:02:31 +0530

    move pagination to top right and show page number

--- src/views/Pagination/Pagination.js
+++ src/views/Pagination/Pagination.js
@@ -10,6 +10,7 @@ const Pagination = () => {
   const [nextToken, setNextToken] = useState(undefined);
   const [nextNextToken, setNextNextToken] = useState();
   const [previousTokens, setPreviousTokens] = useState([]);
+  const [currentPage, setCurrentPage] = useState(1);
 
   useEffect(() => {
     fetch('https://af5xufo4j6.execute-api.us-east-1.amazonaws.com/test/all', {
@@ -26,11 +27,15 @@ const Pagination = () => {
   }, [nextToken]);
 
   const next = () => {
+    setCurrentPage(currentPage + 1);
     setPreviousTokens((c) => [...c, nextToken]);
     setNextToken(nextNextToken);
   };
 
   const prev = () => {
+    if (currentPage > 1) {
+      setCurrentPage(currentPage - 1);
+    }
     const copy = [...previousTokens];
     const b = copy.pop();
     setPreviousTokens(copy);
@@ -41,18 +46,19 @@ const Pagination = () => {
       <Grid container spacing={0} className="main_container">
         <Grid item xs={12} sm={12} md={2} lg={2}></Grid>
         <Grid item xs={12} sm={12} md={8} lg={8} style={{ marginTop: '3%' }}>
-          {list.map((item,index) => {
-            return <ToDoItem id={Math.random()} timeout={250 * (index + 1)} name={item.PK} date={item.SK} />;
-          })}
           <div id="btn_container">
-            <Button variant="contained" onClick={prev} color="primary">
+            <div style={{ flexGrow: 1 }} />
+            <Button variant="contained" onClick={prev} color="primary" disabled={currentPage === 1}>
               prev
             </Button>
-            <div style={{ flexGrow: 1 }} />
+            <p id="current_page">{currentPage}</p>
             <Button variant="contained" onClick={next} color="primary">
               next
             </Button>
           </div>
+          {list.map((item, index) => {
+            return <ToDoItem id={Math.random()} timeout={250 * (index + 1)} name={item.PK} date={item.SK} />;
+          })}
         </Grid>
       </Grid>
     </div>
