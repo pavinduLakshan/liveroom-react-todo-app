51a298e73928b9200bbbd2b342729ff265155ed5
pavinduLakshan <174084F@uom.lk>
2020-07-14 17:09:50 +0530

    remove env files, browserrouter and env

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from 'react';
 import CssBaseline from '@material-ui/core/CssBaseline';
 import { MuiThemeProvider, createMuiTheme } from '@material-ui/core/styles';
 // Other imports
-import { BrowserRouter, Route } from 'react-router-dom';
+import { HashRouter, Route } from 'react-router-dom';
 import ToDoList from './views/TodoList';
 import Pagination from './views/Pagination/Pagination';
 import './App.css';
@@ -17,15 +17,15 @@ const themeLight = createMuiTheme({
 
 function App() {
   return (
-    <BrowserRouter basename={process.env.REACT_APP_BASE_URL}>
+    <HashRouter basename="/liveroom-react-todo-app">
       <MuiThemeProvider theme={themeLight}>
         <CssBaseline />
         <div className="App">
-          <Route exact path="/" component={Pagination} />
-          <Route path="/pagination" component={ToDoList} />
+          <Route exact path="/" component={ToDoList} />
+          <Route path="/pagination" component={Pagination} />
         </div>
       </MuiThemeProvider>
-    </BrowserRouter>
+    </HashRouter>
   );
 }
 
