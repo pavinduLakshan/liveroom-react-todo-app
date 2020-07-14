39e3ed849eced82929c8decb42843f7170803244
pavinduLakshan <174084F@uom.lk>
2020-07-14 11:49:56 +0530

    add material ui specific background color to root

--- src/App.js
+++ src/App.js
@@ -5,7 +5,8 @@ import AppBar from '@material-ui/core/AppBar';
 import Grid from '@material-ui/core/Grid';
 import Toolbar from '@material-ui/core/Toolbar';
 import Typography from '@material-ui/core/Typography';
-
+import CssBaseline from '@material-ui/core/CssBaseline';
+import { MuiThemeProvider, createMuiTheme } from '@material-ui/core/styles';
 // Other imports
 import './App.css';
 import ToDoItem from './components/ToDoItem';
@@ -16,6 +17,14 @@ const useStyles = makeStyles((theme) => ({
     flexGrow: 1,
   },
 }));
+
+const themeLight = createMuiTheme({
+  palette: {
+    background: {
+      default: '#D4E0E4',
+    },
+  },
+});
 
 function App() {
   const classes = useStyles();
@@ -29,9 +38,9 @@ function App() {
 
   function setCompletedStatus(id, newStatus) {
     let selectedItem = toDos.find((todo) => todo.id === id);
-    if(!selectedItem){
-      alert("Item doesn't exist.")
-      return
+    if (!selectedItem) {
+      alert("Item doesn't exist.");
+      return;
     }
     selectedItem.completed = newStatus;
 
@@ -45,60 +54,65 @@ function App() {
   }
 
   return (
-    <div className="App">
-      <div>
-        {/* AppBar */}
-        <AppBar position="static">
-          <Toolbar>
-            <Typography variant="h6" className={classes.title}>
-              The LiveRoom Todo App
-            </Typography>
-          </Toolbar>
-        </AppBar>
-        <AddTaskForm onSubmit={addToDo} />
-        {/* Main Container */}
-        <Grid container spacing={0} className="main_container">
-          {/* To do work */}
-          <Grid item xs={12} sm={12} md={6} lg={6} style={{ minHeight: '20vh' }}>
-            <Typography variant="h5" component="h5" style={{ textAlign: 'left' }}>
-              To do
-            </Typography>
-            {toDos
-              .filter((item) => !item.completed)
-              .map((item) => (
-                <ToDoItem
-                  id={item.id}
-                  name={item.name}
-                  date={item.date}
-                  time={item.time}
-                  onChangeStatus={setCompletedStatus}
-                  onDelete={deleteToDo}
-                  isCompleted={item.completed}
-                />
-              ))}
+    <MuiThemeProvider theme={themeLight}>
+      <CssBaseline />
+
+      <div className="App">
+        <div>
+          {/* AppBar */}
+          <AppBar position="static">
+            <Toolbar>
+              <Typography variant="h6" className={classes.title}>
+                The LiveRoom Todo App
+              </Typography>
+            </Toolbar>
+          </AppBar>
+
+          {/* Main Container */}
+          <Grid container spacing={0} className="main_container">
+            {/* To do work */}
+            <Grid item xs={12} sm={12} md={6} lg={6} style={{ minHeight: '40vh', padding: '1%', paddingRight: '1%' }}>
+              <Typography variant="h5" component="h5" style={{ textAlign: 'left', marginBottom: '1%' }}>
+                To do
+              </Typography>
+              {toDos
+                .filter((item) => !item.completed)
+                .map((item) => (
+                  <ToDoItem
+                    id={item.id}
+                    name={item.name}
+                    date={item.date}
+                    time={item.time}
+                    onChangeStatus={setCompletedStatus}
+                    onDelete={deleteToDo}
+                    isCompleted={item.completed}
+                  />
+                ))}
+            </Grid>
+            {/* Completed tasks */}
+            <Grid item xs={12} sm={12} md={6} lg={6} style={{ minHeight: '40vh', padding: '1%', paddingRight: '1%' }}>
+              <Typography variant="h5" component="h5" style={{ textAlign: 'left', marginBottom: '1%' }}>
+                Done
+              </Typography>
+              {toDos
+                .filter((item) => item.completed)
+                .map((item) => (
+                  <ToDoItem
+                    id={item.id}
+                    name={item.name}
+                    date={item.date}
+                    time={item.time}
+                    onChangeStatus={setCompletedStatus}
+                    onDelete={deleteToDo}
+                    isCompleted={item.completed}
+                  />
+                ))}
+            </Grid>
           </Grid>
-          {/* Completed tasks */}
-          <Grid item xs={12} sm={12} md={6} lg={6} style={{ minHeight: '20vh' }}>
-            <Typography variant="h5" component="h5" style={{ textAlign: 'left' }}>
-              Done
-            </Typography>
-            {toDos
-              .filter((item) => item.completed)
-              .map((item) => (
-                <ToDoItem
-                  id={item.id}
-                  name={item.name}
-                  date={item.date}
-                  time={item.time}
-                  onChangeStatus={setCompletedStatus}
-                  onDelete={deleteToDo}
-                  isCompleted={item.completed}
-                />
-              ))}
-          </Grid>
-        </Grid>
+          <AddTaskForm onSubmit={addToDo} />
+        </div>
       </div>
-    </div>
+    </MuiThemeProvider>
   );
 }
 
