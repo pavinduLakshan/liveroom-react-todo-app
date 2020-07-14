a51a49c3ed08688da653d2cb818191652cdce9a6
pavinduLakshan <174084F@uom.lk>
2020-07-14 09:53:51 +0530

    format code according to prettier config

--- src/components/AddTaskForm.js
+++ src/components/AddTaskForm.js
@@ -1,22 +1,18 @@
-import React, { useState } from "react";
-import Modal from "@material-ui/core/Modal";
-import TextField from "@material-ui/core/TextField";
-import Button from "@material-ui/core/Button";
-import Typography from "@material-ui/core/Typography";
-import CloseIcon from "@material-ui/icons/Close";
+import React, { useState } from 'react';
+import Modal from '@material-ui/core/Modal';
+import TextField from '@material-ui/core/TextField';
+import Button from '@material-ui/core/Button';
+import Typography from '@material-ui/core/Typography';
+import CloseIcon from '@material-ui/icons/Close';
 // date and time utils
-import "date-fns";
-import {
-  MuiPickersUtilsProvider,
-  KeyboardTimePicker,
-  KeyboardDatePicker,
-} from "@material-ui/pickers";
-import DateFnsUtils from "@date-io/date-fns";
+import 'date-fns';
+import { MuiPickersUtilsProvider, KeyboardTimePicker, KeyboardDatePicker } from '@material-ui/pickers';
+import DateFnsUtils from '@date-io/date-fns';
 
 const AddTaskForm = ({ onSubmit }) => {
   const [isOpen, setOpen] = useState(false);
   //form values
-  const [name, setName] = useState("");
+  const [name, setName] = useState('');
   const [date, setDate] = React.useState(new Date());
   const [time, setTime] = React.useState(new Date());
 
@@ -43,13 +39,13 @@ const AddTaskForm = ({ onSubmit }) => {
   function handleSubmit(e) {
     e.preventDefault();
     onSubmit({
-        id: Math.random(),
+      id: Math.random(),
       name,
       date: date.toLocaleDateString(),
       time: time.toLocaleTimeString(),
-      completed: false
+      completed: false,
     });
-    handleClose()
+    handleClose();
   }
 
   return (
@@ -65,24 +61,12 @@ const AddTaskForm = ({ onSubmit }) => {
         aria-describedby="simple-modal-description"
       >
         <div className="add-todo-modal">
-          <CloseIcon
-            onClick={handleClose}
-            style={{ float: "right", marginBottom: "3%" }}
-          />
-          <Typography variant="h6" style={{ marginBottom: "4%" }}>
+          <CloseIcon onClick={handleClose} style={{ float: 'right', marginBottom: '3%' }} />
+          <Typography variant="h6" style={{ marginBottom: '4%' }}>
             Add new Todo
           </Typography>
-          <form
-            onSubmit={handleSubmit}
-            style={{ display: "flex", flexDirection: "column" }}
-          >
-            <TextField
-              id="outlined-basic"
-              label="name"
-              required
-              variant="outlined"
-              onChange={handleNameChange}
-            />
+          <form onSubmit={handleSubmit} style={{ display: 'flex', flexDirection: 'column' }}>
+            <TextField id="outlined-basic" label="name" required variant="outlined" onChange={handleNameChange} />
             <MuiPickersUtilsProvider utils={DateFnsUtils}>
               <KeyboardDatePicker
                 margin="normal"
@@ -93,7 +77,7 @@ const AddTaskForm = ({ onSubmit }) => {
                 value={date}
                 onChange={handleDateChange}
                 KeyboardButtonProps={{
-                  "aria-label": "change date",
+                  'aria-label': 'change date',
                 }}
               />
               <KeyboardTimePicker
@@ -104,14 +88,11 @@ const AddTaskForm = ({ onSubmit }) => {
                 required
                 onChange={handleTimeChange}
                 KeyboardButtonProps={{
-                  "aria-label": "change time",
+                  'aria-label': 'change time',
                 }}
               />
             </MuiPickersUtilsProvider>
-            <Button
-              type="submit"
-              style={{ backgroundColor: "green", color: "white" }}
-            >
+            <Button type="submit" style={{ backgroundColor: 'green', color: 'white' }}>
               <Typography variant="h6">Create</Typography>
             </Button>
           </form>
